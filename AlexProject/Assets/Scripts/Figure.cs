b9eff0868dad454bfe641669191f4c8478e95ab1
chivoda10 <chivoda10@gmail.com>
2020-04-28 16:34:39 +0400

    Initial commit

---
+++ AlexProject/Assets/Scripts/Figure.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Figure : MonoBehaviour
+{
+    private float maxScale = 2f;
+    private MeshRenderer meshRenderer;
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        Changes();
+    }
+
+    private void Changes()
+    {
+        meshRenderer.material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        float randomScale = Random.Range(0.5f, maxScale);
+        transform.localScale += new Vector3(randomScale, randomScale, randomScale);
+    }
+}
---
+++ AlexProject/Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] figures;
+
+    [Header("Position X")]
+    [SerializeField] private float minPosX;
+    [SerializeField] private float maxPosX;
+
+    [Header("Position Y")]
+    [SerializeField] private float minPosY;
+    [SerializeField] private float maxPosY;
+
+    [Header("Position Z")]
+    [SerializeField] private float minPosZ;
+    [SerializeField] private float maxPosZ;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            int currentFigureNum = Random.Range(0, figures.Length);
+            float randomPosX = Random.Range(minPosX, maxPosX);
+            float randomPosY = Random.Range(minPosY, maxPosY);
+            float randomPosZ = Random.Range(minPosZ, maxPosZ);
+
+            GameObject thisFigure = Instantiate(figures[currentFigureNum], new Vector3(randomPosX, randomPosY, randomPosZ), Quaternion.identity);
+        }
+    }
+}
