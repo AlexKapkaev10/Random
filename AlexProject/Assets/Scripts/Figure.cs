56912c074cecccfae264c74b90cd428274345a96
chivoda10 <chivoda10@gmail.com>
2020-04-29 08:58:02 +0400

    add list gameobject

--- AlexProject/Assets/Scripts/Figure.cs
+++ AlexProject/Assets/Scripts/Figure.cs
@@ -3,17 +3,16 @@
 public class Figure : MonoBehaviour
 {
     private float maxScale = 2f;
-    private MeshRenderer meshRenderer;
-
+    private MeshRenderer _meshRenderer;
     void Start()
     {
-        meshRenderer = GetComponent<MeshRenderer>();
+        _meshRenderer = GetComponent<MeshRenderer>();
         Changes();
     }
 
     private void Changes()
     {
-        meshRenderer.material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        _meshRenderer.material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
         float randomScale = Random.Range(0.5f, maxScale);
         transform.localScale += new Vector3(randomScale, randomScale, randomScale);
     }
--- AlexProject/Assets/Scripts/Spawner.cs
+++ AlexProject/Assets/Scripts/Spawner.cs
@@ -1,31 +1,37 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
     public GameObject[] figures;
+    public List<GameObject> SaverFigures;
 
     [Header("Position X")]
-    [SerializeField] private float minPosX;
-    [SerializeField] private float maxPosX;
+    [SerializeField] private float _minPositionX;
+    [SerializeField] private float _maxPositionX;
 
     [Header("Position Y")]
-    [SerializeField] private float minPosY;
-    [SerializeField] private float maxPosY;
+    [SerializeField] private float _minPositionY;
+    [SerializeField] private float _maxPositionY;
 
     [Header("Position Z")]
-    [SerializeField] private float minPosZ;
-    [SerializeField] private float maxPosZ;
+    [SerializeField] private float _minPositionZ;
+    [SerializeField] private float _maxPositionZ;
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            
             int currentFigureNum = Random.Range(0, figures.Length);
-            float randomPosX = Random.Range(minPosX, maxPosX);
-            float randomPosY = Random.Range(minPosY, maxPosY);
-            float randomPosZ = Random.Range(minPosZ, maxPosZ);
-
+            float randomPosX = Random.Range(_minPositionX, _maxPositionX);
+            float randomPosY = Random.Range(_minPositionY, _maxPositionY);
+            float randomPosZ = Random.Range(_minPositionZ, _maxPositionZ);
+            
             GameObject thisFigure = Instantiate(figures[currentFigureNum], new Vector3(randomPosX, randomPosY, randomPosZ), Quaternion.identity);
+            SaverFigures.Add(thisFigure);
+            
+
         }
     }
 }
